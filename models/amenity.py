f21cb230c51ef1d7c3b7e8c48867b1a39d08ae9a
CodeHunter392 <omarbenamer392@gmail.com>
2024-03-02 06:49:26 -0800

    airbnb project tasks wq

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module creates The Amenity class"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """This Class is for managing amenity objects"""
+
+    name = ""
